0a346824307881b83a31b2e0fee0f26be6a86d2a
Zeeyad Monir <zeeyadmonir@gmail.com>
2025-09-25 23:15:24 -0700

    update

--- src/screens/ActiveCompetitionsScreen.js
+++ src/screens/ActiveCompetitionsScreen.js
@@ -1544,7 +1544,7 @@ const styles = StyleSheet.create({
   },
   
   statusBadgeResults: {
-    backgroundColor: '#FFC107', // Yellow/amber for results
+    backgroundColor: '#5A8A2E', // Darker green for results
   },
 
   statusBadgeText: {
